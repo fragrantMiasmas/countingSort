0f73c47c721a8ac423ecf425931ff0e3e75f67fc
fragrantMiasmas <exreed@willamette.edu>
2017-03-15 20:55:11 -0700

    Counting Sort, with stopwatch.

---
+++ src/counting/sort/CountingSort.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package counting.sort;
+
+/**
+ *
+ * @author ElizabethReed PC
+ */
+public class CountingSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+//        A = [3,1,6,2,1,3,5,1];
+//
+//% A = [2,5,3,0,2,3,0,3];
+//
+//[m,n] = size(A);
+//B = [];
+//C = [];
+//
+//k = max(A);
+//
+//% must change slightly bc matlab doesn't have zero index
+//
+//for i = 1:k
+//    C(i) = 0;
+//end
+//
+//for j = 1:n
+//    C(A(j)) = C(A(j)) + 1;
+//end
+//
+//% for i = 1:k
+//%     if i == 1
+//%         C(i) = C(i);
+//%     else
+//%         C(i) = C(i) + C(i-1);
+//%     end
+//%     
+//% end
+//
+//% for i = n:1
+//%     B(C(A(j))) = A(j);
+//%     C(A(j)) = C(A(j)) - 1;
+//% end
+        
+        
+        
+        
+        
+    }
+    
+}
